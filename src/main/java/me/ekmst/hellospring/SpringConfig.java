9a50f18bb8aebf3bacaa1455f07128b8bbb0d7ae
kyeongmin <kyeongmin.jake.lee@gmail.com>
2020-10-04 16:06:31 +0900

    순수 JDBC

--- src/main/java/me/ekmst/hellospring/SpringConfig.java
+++ src/main/java/me/ekmst/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package me.ekmst.hellospring;
 
+import me.ekmst.hellospring.repository.JdbcMemberRepository;
 import me.ekmst.hellospring.repository.MemberRepository;
 import me.ekmst.hellospring.repository.MemoryMemberRepository;
 import me.ekmst.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
